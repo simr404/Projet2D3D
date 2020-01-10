d3a77b97851294afbf367b54890db03f4ba58af2
bloodeath <anticreeper246@gmail.com>
2020-01-10 16:46:36 +0100

    0.1 - Script camera
    ajout d'un script pour déplacé la caméra pour suivre un transform

---
+++ Assets/Script/CCamera3d.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CCamera3d : MonoBehaviour
+{
+    public Transform center;
+    public Transform player;
+    public float height = 4.0f;
+    public float offset = 7.0f;
+
+    private void Update()
+    {
+        cameraMove();
+    }
+    // la caméra se déplace afin de voir le joueur 
+    private void cameraMove()
+    {
+        Vector3 dir = new Vector3(player.position.x - center.position.x, 0, player.position.z - center.position.z);
+        transform.position = dir.normalized * offset + Vector3.up * height;
+        transform.LookAt(player);
+    }
+}
