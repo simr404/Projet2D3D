6377172ba822b7679ad0cfa61615b6a6bf3ccba1
Louis GRANGE <46597365+Kraknistic@users.noreply.github.com>
2020-01-10 15:20:22 +0100

    Movement2D

---
+++ Assets/Script/Movement2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    public KeyCode KCLeft;
+    public KeyCode KCRight;
+    public float fSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KCRight))
+            gameObject.transform.Translate(Vector3.right * Time.deltaTime * fSpeed);
+        if (Input.GetKey(KCLeft))
+            gameObject.transform.Translate(Vector3.left * Time.deltaTime * fSpeed);
+    }
+}
