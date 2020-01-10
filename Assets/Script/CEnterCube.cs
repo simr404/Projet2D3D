d0b557d51526114f1ec7f7792720d865813a3c8f
RinalduzziNathan <n.rinalduzzi@gmail.com>
2020-01-10 15:44:46 +0100

    Ajout CEnterCube.cs
    Ajout fichier CEnterCube.cs qui permet de rentrer depuis le monde 3D dans le monde 2D
    
    Duplication de la scene pour ne pas cause de problemes

---
+++ Assets/Script/CEnterCube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CEnterCube : MonoBehaviour
+{
+    //Author : Rinalduzzi
+
+
+    public GameObject m_Player2D;//reference to the 2D sprite character
+    public GameObject m_Player3D;//reference to the 3D character
+
+    private void Start()
+    {
+        m_Player2D.SetActive(false);
+        m_Player3D.SetActive(true);        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == m_Player3D.name)//verify that the object who triggered the hitbox is the player
+        {
+            m_Player3D.gameObject.SetActive(false);
+            m_Player2D.gameObject.SetActive(true);
+        }
+    }
+
+
+}
